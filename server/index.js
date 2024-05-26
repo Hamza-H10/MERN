9b3fdd35d52e6826e5608665c8ffdd46c3da0151
Hamza Haider <123259911+Hamza-H10@users.noreply.github.com>
2024-05-27 02:11:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://root:root@cluster0.lodlqbq.mongodb.net/CRUD', {
+mongoose.connect('mongodb+srv://root:root@cluster0.lodlqbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     // useNewUrlParser: true,
     // useUnifiedTopology: true
 }).then(() => {
